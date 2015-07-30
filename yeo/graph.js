3da1896645772147f65cd47b52ac8f226ec79e58
beomki <yeobk1202@kaist.ac.kr>
2015-07-30 16:40:58 +0200

    Tired... commit clones

---
+++ yeo/graph.js
@@ -0,0 +1,21 @@
+function processData(nationData){
+}
+
+
+d3.json("./2015-07-27-data/d3/nations.json", processData);
+
+
+
+
+
+/*  no need to use it anymore
+
+alert("Hello!");
+var list_of_num = [1,2,10];
+alert(list_of_num);
+console.log(list_of_num[0]);
+
+var an_object = [[1,2], [5,6], {"a": 121, "b": "hello"}];
+console.log(an_object);		
+
+*/
